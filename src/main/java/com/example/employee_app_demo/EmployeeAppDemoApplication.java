3894ef0d61f6517fe0bb2f2c7ddb13c67d9b710e
Qingyang Zhang <zqy1jason@gmail.com>
2024-11-30 17:30:36 -0500

    demo project committed

---
+++ src/main/java/com/example/employee_app_demo/EmployeeAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_app_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAppDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employee_app_demo/EmployeeAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_app_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
